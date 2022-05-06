d60164d7341a0924ed2b3049ffd9039b74a6f2a4
Neha Haq <nwhaq1@berkeley.edu>
2022-05-06 01:03:38 +0000

    added pytest

--- diagnosis/twosample.py
+++ diagnosis/twosample.py
@@ -1,24 +1,29 @@
 from scipy import stats
 import matplotlib.pyplot as plt
 
-# Plots the count distribution of the column specified for those with cancer vs. those without. The input data must have a diagnosis column. 
-# The output is a plot.
+
 def plt_by_diagnosis(dat, column, name):
-    dat["diagnosis"].replace({0: "No Cancer", 1: "Cancer"}, inplace = True)
-    cols = dat["diagnosis"].unique();
+    # Plots the count distribution of the column specified for those with cancer vs. 
+    # those without. 
+    # The input data must have a diagnosis column. 
+    # The output is a plot.
+    cols = dat["diagnosis"].unique()
     plt.hist([dat.loc[dat["diagnosis"] == x, column] for x in cols], label=cols);
     plt.legend()
     plt.title(name + " by Diagnosis");
     plt.xlabel(name)
     plt.ylabel("Count");
     plt.savefig('../figures/' + column)
-    
 
-    
-# Conducts the two sample t test for the data where the two populations are segregated by the "by" input. The input feature is the featuree that we want to test whether it has the sample value or not in the two populations. The var is boolean signalling whether the two populations have the same variance or not. 
 
-# The output is the t statistics, p value, and the conclusion of the two sample t test.
 def two_sample_t_test(dat, by, feature, var):
+    # Conducts the two sample t test for the data where the two populations are
+    # segregated by the "by" input. The input feature is the featuree that we
+    # want to test whether it has the sample value or not in the two populations.
+    # The var is boolean signalling whether the two populations have the
+    # same variance or not. 
+    # # The output is the t statistics, p value.
+    
     dat_0 = dat.loc[dat[by] == 0]
     dat_1 = dat.loc[dat[by] == 1]
     results = stats.ttest_ind(dat_0[feature], dat_1[feature], equal_var=var)
