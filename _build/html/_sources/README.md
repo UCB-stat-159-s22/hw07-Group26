49db2f00fb4e0bb3977b35b3aa33c46f4311d556
Neha Haq <nwhaq1@berkeley.edu>
2022-05-09 04:38:28 +0000

    jupyter book, added comments to two sample

---
+++ _build/html/_sources/README.md
@@ -0,0 +1,46 @@
+# A Study of a Breast Cancer Dataset
+
+TODO: Binder
+
+Authors: Kshitij (TJ) Chauhan, Neha Haq, Wenhao Pan, Jiaji Wu
+
+## Introduction
+
+This repository includes a study of a breast bancer dataset that is downloaded from [Kaggle](https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data)
+
+TODO
+
+## Installation
+
+TODO
+
+## Repository Structure
+
+- `data/` contains different datasets in csv formats
+  - `raw_data.csv` is the original data downloaded from Kaggle
+  - `clean.csv` is the cleaned version of `raw_data.csv`
+  - `train.csv` is the training dataset 
+  - `val.csv` is the validation dataset
+  - `test.csv` is the testing dataset
+- `figures/` contains generated figures
+- `tables/` contains generated tables
+- `codes/` contains codes for data analysis
+  - `prepare.ipynb` prepares the data for later analysis
+  - `EDA.ipynb` conducts EDA 
+- `environment.yml`
+- `envsetup.sh`
+- `envupdate.sh`
+- `html_hub.sh`
+- `LICENSE`
+- `Makefile`
+- `README.md`
+- `requirements.txt`
+- `main.ipynb` summarizes and discusses the findings and outcomes of our analysis
+
+## Makefile Commands
+
+TODO
+
+## Notes
+
+TODO
--- diagnosis/twosample.py
+++ diagnosis/twosample.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 
 
 def plt_by_diagnosis(dat, column, name):
+    """
+    Plot the comparitive count distribution of the column specified for those 
+    diagnosed vs. those not.
+    ---
+    Arguments: 
+    dat (pandas dataframe): the dataframe which contains two columns, a diagnosis 
+                            column and a column to plot the distribution of. 
+    column (str): name of the column, which is a int, float type, to plot the 
+                  distribution of.
+    name (str): label the title of the plot
+    ---
+    Returns 
+    None, plots the figure.
+    Additionally, saves the figure under the figures folder.
+    """
     # Plots the count distribution of the column specified for those with cancer vs. 
     # those without. 
     # The input data must have a diagnosis column. 
@@ -17,6 +32,22 @@ def plt_by_diagnosis(dat, column, name):
 
 
 def two_sample_t_test(dat, by, feature, var):
+    """
+    Conducts the two sample t-test for two populations.
+    ---
+    Arguments: 
+    dat (pandas dataframe): the dataframe which contains two columns, a 'by' 
+                            column and a 'feature' column.
+    by (str): name of the column, which is of binary type (0, 1), to segregate 
+              the two populations by. 
+    feature (str): name of the column, which is a int, float type, to compute the 
+                   population parameter difference.
+    var (boolean): 'True' or 'False' as to whether the two populations have the same
+                    population variance or not.
+    ---
+    Returns tuple containing the (t-statistic, p-value)
+    Prints the conclusion of the hypothesis test.
+    """
     # Conducts the two sample t test for the data where the two populations are
     # segregated by the "by" input. The input feature is the featuree that we
     # want to test whether it has the sample value or not in the two populations.
