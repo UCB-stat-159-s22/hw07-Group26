2b67322e3636be9cb9924cc8b98d336e48477a00
Wenhao Pan <wenhao1102@berkeley.edu>
2022-05-11 20:59:05 +0000

    Jupyterbook

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 author = 'The Jupyter Book Community & Neha, Wenhao, TJ, Jiaji'
 bibtex_bibfiles = ['references.bib']
 comments_config = {'hypothesis': False, 'utterances': False}
-copyright = '2021'
+copyright = '2022'
 exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
 execution_allow_errors = False
 execution_excludepatterns = []
@@ -21,7 +21,7 @@ html_logo = 'logo.png'
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
 html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 'https://github.com/executablebooks/jupyter-book', 'repository_branch': 'master', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'use_repository_button': True, 'use_edit_page_button': False, 'use_issues_button': True}
-html_title = 'My STAT 159 Homework 7 Book'
+html_title = 'Breast Cancer Study'
 jupyter_cache = ''
 jupyter_execute_notebooks = 'off'
 language = None
